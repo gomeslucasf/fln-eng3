0b264039eee60a926f06af90758dae3f7abdbf59
Felipe Geroldi <feriip3d@gmail.com>
2021-04-05 17:45:05 -0300

    Observer p3

---
+++ FLNControlENG3/Models/Observador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLNControlENG3.Models
+{
+    public interface Observador
+    {
+        void atualizar(string acao);
+    }
+}
